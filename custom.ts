2ec33a3a31af8fd96ae7e6f1d24684b3f0e872f1
aha ha Crabronidae <ahawespe@gmail.com>
2022-08-30 11:25:57 +0200

    added a slider for interval

--- custom.ts
+++ custom.ts
@@ -4,7 +4,6 @@ let animation_running: boolean
 * Weitere Informationen unter https://makecode.microbit.org/blocks/custom
 */
 
-
 /**
  * Custom blocks
  */
@@ -14,12 +13,14 @@ namespace flipbook {
      * Creates a Animation with a array of Images.
      * @param n describe parameter here, eg: 20
      */
-    //% block="Animiere $a mit $n FPS"
+    //% block="Animiere $a alle $n ms"
+    //% n.min=1 n.max=10000
+    //% n.fieldOptions.precision=1
     export function animate(a: Image[], n: number): void {
         animation_running = true
         for (let Index = 0; Index <= a.length - 1; Index++) {
             a[Index].showImage(0)
-            basic.pause((1 / n) * 1000)
+            basic.pause(n)
         }
         animation_running = false
         basic.clearScreen()
@@ -27,11 +28,19 @@ namespace flipbook {
 
 
     /**
-     * TODO: describe your function here
-     * @param value describe value here, eg: 5
+     * returns true if an animation is running.
      */
     //% block="Läuft eine Animation?"
     export function isrunning(): boolean {
         return animation_running
     }
+
+    /**
+     * Show an image with many effects.
+     */
+    //% block=""
+    export function animateimg(img: Image): void {
+
+    }
 }
+
